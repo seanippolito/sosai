4d6089f947733f7ed556b89e71968a99c4aa5ec6
Sean Ippolito <ippolitosean@gmail.com>
2025-03-21 19:34:55 -0700

    update the sign-in sign-up pages UI and implement forgot password, tested locally

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -3,8 +3,10 @@ import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
-    request,
-  })
+    request: {
+      headers: request.headers,
+    },
+  });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -31,10 +33,12 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user && !request.nextUrl.pathname.startsWith("/login")) {
-    const url = request.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
+  if (!user && !request.nextUrl.pathname.startsWith("/protected")) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  if (request.nextUrl.pathname === "/" && !user) {
+    return NextResponse.redirect(new URL("/protected", request.url));
   }
 
   return supabaseResponse
