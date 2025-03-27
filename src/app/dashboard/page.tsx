c0fa03cae5cb768f70c95e24e7bdc08e7121d0bf
Sean Ippolito <ippolitosean@gmail.com>
2025-03-26 20:06:10 -0700

    code clean up and UI fixes, link navigation improvement

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,16 @@
 import { createClient } from '@/utils/supabase/server';
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
   const supabase = await createClient();
-  const { data: user } = await supabase.auth.getUser();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
 
   return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
--- src/components/ui/dashboard/sidenav.tsx
+++ src/components/ui/dashboard/sidenav.tsx
@@ -7,15 +7,15 @@ export default function SideNav() {
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-        href="/"
+        href="/dashboard"
       >
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
         </div>
       </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="flex grow flex-row justify-between space-x-2">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <div className="hidden h-auto w-full grow bg-gray-50"></div>
       </div>
     </div>
   );
